0f390cc1eb0677a7f35d0e5f21deac81a9ffb1e1
Jacob Smit <jacobsmit@shaw.ca>
2024-01-02 22:42:06 -0800

    train data and determine accuracy

--- mitbih.py
+++ mitbih.py
@@ -3,6 +3,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+# number of cycles through all training data
+NUM_EPOCHS = 1000
+
+# process data into training and test; X and y
 print("Preparing data...")
 train_data = np.genfromtxt("mitbih_train.csv", delimiter=",")
 X_train = torch.tensor(train_data[:,:-1], dtype=torch.float32)
@@ -12,6 +16,7 @@ test_data = np.genfromtxt("mitbih_test.csv", delimiter=",")
 X_test = torch.tensor(test_data[:,:-1], dtype=torch.float32)
 y_test = torch.tensor(test_data[:,-1], dtype=torch.long)
 
+# define the class with layers and activation functions
 print("Initalizing model...")
 class MITBIHClassifier(nn.Module) :
     def __init__(self) :
@@ -28,7 +33,27 @@ class MITBIHClassifier(nn.Module) :
         x = self.out(x)
         return x
     
+# initalize the model, loss function, and optimizer
 model = MITBIHClassifier()
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+# train model on training data for NUM_EPOCHS
+print("Training model...")
+for epoch in range(NUM_EPOCHS) :
+    y_pred = model(X_train)
+    loss = loss_fn(y_pred, y_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(f'Finished epoch {epoch}, latest loss {loss}')
+
+# determine accuracy via test data
+with torch.no_grad :
+    model.eval()
+    y_pred = model(X_test)
+    model.train()
+
+y_pred = torch.argmax(y_pred, dim=1)
+accuracy = (y_pred == y_test).float().mean()
+print(f"Test Accuracy: {accuracy_test}")
