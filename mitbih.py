d88c74128c8754ca75babfc68fdb776737d7223f
Jacob Smit <jacobsmit@shaw.ca>
2024-01-02 20:34:21 -0800

    set loss function and optimizer

--- mitbih.py
+++ mitbih.py
@@ -11,3 +11,24 @@ y_train = torch.tensor(train_data[:,-1], dtype=torch.long)
 test_data = np.genfromtxt("mitbih_test.csv", delimiter=",")
 X_test = torch.tensor(test_data[:,:-1], dtype=torch.float32)
 y_test = torch.tensor(test_data[:,-1], dtype=torch.long)
+
+print("Initalizing model...")
+class MITBIHClassifier(nn.Module) :
+    def __init__(self) :
+        super().__init__()
+        self.hidden1 = nn.Linear(187, 100)
+        self.act1 = nn.ReLU()
+        self.hidden2 = nn.Linear(100, 50)
+        self.act2 = nn.ReLU()
+        self.out = nn.Linear(50, 5)
+
+    def forward(self, x) :
+        x = self.act1(self.hidden1(x))
+        x = self.act2(self.hidden2(x))
+        x = self.out(x)
+        return x
+    
+model = MITBIHClassifier()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
